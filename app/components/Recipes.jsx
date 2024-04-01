d5d04273e4cfc86c708910f127d8116029d54980
Iria Moreno Rodriguez <iria.moreno12@gmail.com>
2024-04-01 14:51:25 +0200

    01/04

--- app/components/Recipes.jsx
+++ app/components/Recipes.jsx
@@ -15,9 +15,18 @@ const Recipes = () => {
      
     const router = useRouter()
 
+    const handleClick = () => {
+      document.body.classList.add('highlight-effect');
+
+      setTimeout(() => {
+        document.body.classList.remove('highlight-effect'); 
+      }, 10000);
+      }
+    
+
 
   return (
-    <div className='grid grid-cols-2 gap-4'>
+    <div className='grid grid-cols-3 gap-4'>
       <button /*onClick={handleRecipeChange}*/ className='border-solid'>
 
               <div className='w-width-custom rounded border border-plain-dark-blue bg-transparent text-plain-dark-blue hover:bg-plain-dark-blue hover:text-white'>
@@ -40,6 +49,25 @@ const Recipes = () => {
             <a href='https://github.com/iriamr12/-books-app' target='_blank'>
               <div className='w-width-custom rounded border border-plain-dark-blue bg-transparent text-plain-dark-blue hover:bg-plain-dark-blue hover:text-white'>
                                 <h3 className='relative top-4 right-28 font-bold'>Books-app</h3>
+                                <div className='grid grid-cols-3 gap-4 justify-center items-center mt-12 mb-10'>
+                                      <div className="flex justify-center items-center"><FaReact className="text-white w-8 h-8"/></div>
+                                      <div className="flex justify-center items-center"><IoLogoCss3 className="text-white w-8 h-8"/></div>
+                                      <div className="flex justify-center items-center"><SiPostman className="text-white w-8 h-8"/></div>
+                                      <div className="flex justify-center items-center"><SiMysql className="text-white w-8 h-8"/></div>
+                                      <div className="flex justify-center items-center"><SiExpress className="text-white w-8 h-8"/></div>
+                                      <div className="flex justify-center items-center"><FaNode className="text-white w-8 h-8"/></div>
+                                      <div className="flex justify-center items-center"><TbBrandVscode className="text-white w-8 h-8"/></div>
+                                      <div className="flex justify-center items-center"><IoLogoJavascript className="text-white w-8 h-8"/></div>
+                              </div>
+                          </div> 
+
+            </a>
+              
+          </button>
+          <button onClick={handleClick} className='border-solid'>
+            <a href='https://github.com/iriamr12/cv' target='_blank'>
+              <div className='w-width-custom rounded border border-plain-dark-blue bg-transparent text-plain-dark-blue hover:bg-plain-dark-blue hover:text-white'>
+                                <h3 className='relative top-4 right-28 font-bold'>Profile Website</h3>
                                 <div className='grid grid-cols-3 gap-4 justify-center items-center mt-12 mb-10'>
                                       <div className="flex justify-center items-center"><FaReact className="text-white w-8 h-8"/></div>
                                       <div className="flex justify-center items-center"><IoLogoCss3 className="text-white w-8 h-8"/></div>
